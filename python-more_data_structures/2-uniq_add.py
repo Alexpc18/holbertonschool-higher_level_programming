4c70158d0d8cd05c9e95ec74a57d35ff25886213
Alexpc18 <alexanderpc18@hotmail.com>
2023-11-01 18:10:59 -0700

    fix script 2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,10 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 def uniq_add(my_list=[]):
-    return(sum(set(my_list)))
+    unique_numbers = set()
+    for item in my_list:
+        if isinstance(item, int):
+            unique_numbers.add(item)
+
+    total = sum(unique_numbers)
+
+    return total
