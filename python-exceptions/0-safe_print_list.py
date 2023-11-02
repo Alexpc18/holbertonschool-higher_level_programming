7b733dee357321acb989eb7a9a05409ef890342f
Alexpc18 <alexanderpc18@hotmail.com>
2023-11-01 17:25:45 -0700

    fix script 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    num = 0
-    for i in range(x):
-        try:
-            print(my_list[i], end="")
-            num += 1
-        except IndexError:
-            break
-    print("")
-    Return
+    elements_printed = 0
+
+    try:
+        iterator = iter(my_list)  # Intenta crear un iterador desde la lista
+        for _ in range(x):
+            item = next(iterator)  # Intenta obtener el siguiente elemento
+            print(item, end='')
+            elements_printed += 1
+    except StopIteration:
+        pass  # Manejamos la excepción StopIteration si la lista se agota
+
+    print()  # Agregamos una nueva línea al final
+    return elements_printed
