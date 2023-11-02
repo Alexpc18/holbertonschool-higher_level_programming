598045db7b024f9b168d62554b7118b2b6dfc80b
Alexpc18 <alexanderpc18@hotmail.com>
2023-11-01 18:08:36 -0700

    make executable script 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
-    new_matrix = [row[:] for row in matrix]
-    for idx, row in enumerate(new_matrix):
-        for idx2, col in enumerate(new_matrix):
-            new_matrix[idx][idx2] = row[idx2] ** 2
-    return new_matrix
+    def square(x):
+        return x ** 2 if isinstance(x, int) else x
+
+    result_matrix = list(map(lambda row: list(map(square, row)), matrix))
+    return result_matrix
