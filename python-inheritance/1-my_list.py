ba4ea3bb4271d558ca605f53613d73848651f055
Alexpc18 <alexanderpc18@hotmail.com>
2023-11-26 20:40:37 -0800

    fix script 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-"""Defines an inherited list class MyList."""
+"""
+Defines an inherited list class MyList.
+"""
 
 
 class MyList(list):
-    """Implements sorted printing for the built-in list class."""
+    """
+    Implements sorted printing for the built-in list class.
+    """
 
     def print_sorted(self):
-        """Print a list in sorted ascending order."""
-        print(sorted(self)
+        """
+        Print a list in sorted ascending order.
+        """
+        sorted_list = sorted(self)
+        print(sorted(self))
