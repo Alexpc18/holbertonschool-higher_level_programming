e4b702303bf658409002782989f6631aa1ec5edf
Alexpc18 <alexanderpc18@hotmail.com>
2023-12-17 20:17:10 -0800

    changing script 2 to test

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,21 +1,49 @@
 #!/usr/bin/python3
-"""  
-lists all states from the database hbtn_0e_0_usa 
+"""
+This script makes us connect with Mysql.i
 """
 
 import MySQLdb
-import sys
+from sys import argv
+
+
+def search_state_by_name():
+    """
+    This function connects to MySQL server retrieves and displays
+    all values in state table where name matches
+    with the argument provided.
+    """
+
+    mysql_user = argv[1]
+    mysql_pass = argv[2]
+    mysql_db = argv[3]
+    host_mysql = 'localhost'
+    port_mysql = 3306
+    state_name = argv[4]
+
+    database = MySQLdb.connect(
+        host=host_mysql,
+        port=port_mysql,
+        user=mysql_user,
+        password=mysql_pass,
+        database=mysql_db
+    )
+
+    cursor = database.cursor()
+    query = "SELECT * FROM states WHERE BINARY\
+            name = '{}' ORDER BY id ASC".format(state_name)
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    database.close()
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
-
+    """
+    This validation prevents it from being executed this file
+    """
+    search_state_by_name()
